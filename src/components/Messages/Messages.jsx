7e75a0e026f4d8c280837952d72e734100ab4159
enapolskiy <enapolskiy83@yandex.ru>
2021-10-10 19:48:41 +0700

    1010

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,32 +1,50 @@
 import classes from "./Messages.module.css";
 import {NavLink} from "react-router-dom";
 
-const Messages = (props) => {
+const DialogItem = (props) => {
+    let path = /Messages/ +props.id;
     return (
-        <div className={classes.content}>
-        <div className={classes.messages}>
-            <div className={classes.messagesItem}>
-                <div className={classes.senderMessage}>
-                    <NavLink to="/Messages/1" activeClassName={classes.activeLink}>Sonya</NavLink>
-                </div>
-                <div className={classes.senderMessage}>
-                    <NavLink to="/Messages/2" activeClassName={classes.activeLink}>John</NavLink>
-                </div>
-                <div className={classes.senderMessage}>
-                    <NavLink to="/Messages/3" activeClassName={classes.activeLink}>Mike</NavLink>
-                        </div>
-                <div className={classes.senderMessage}>
-                    <NavLink to="/Messages/4" activeClassName={classes.activeLink}>Vincent</NavLink>
-                </div>
-            </div>
-            <div className={classes.messageItem}>
-                <div className={classes.message}>Hi! How are you?</div>
-                <div className={classes.message}>Im fine. what about you? </div>
-                <div className={classes.message}>Oy! im very glad to see you</div>
-                <div className={classes.message}>Cool site!!</div>
-            </div>
-        </div>
+        <div className={classes.senderMessage}>
+            <NavLink to={path} activeClassName={classes.activeLink}>{props.name}</NavLink>
         </div>
     )
 }
-export  default Messages;
+
+const MessageItem = (props) => {
+    return (
+        <div className={classes.message}>{props.message}</div>
+    )
+}
+
+const Messages = (props) => {
+    let sendData = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "John"},
+        {id: 3, name: "Mike"},
+        {id: 4, name: "Vincent"},
+        {id: 5, name: "Sonya"},
+    ];
+    let messageData = [
+        {id: 1, message: "Hi! How are you?"},
+        {id: 2, message: "Im fine. what about you?"},
+        {id: 3, message: "Oy! im very glad to see you"},
+        {id: 4, message: "Cool site!!"},
+        {id: 5, message: "Cool site eah!!"}
+        ]
+
+    let sendElement = sendData.map (send =>  <DialogItem name={send.name} id={send.id}/>)
+    let messageElement = messageData.map (mes => <MessageItem message={mes.message} />)
+    return (
+        <div className={classes.content}>
+            <div className={classes.messages}>
+                <div className={classes.messagesItem}>
+                    {sendElement}
+                </div>
+                <div className={classes.messageItem}>
+                    {messageElement}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Messages;
--- src/components/MyPosts/MyPosts.jsx
+++ src/components/MyPosts/MyPosts.jsx
@@ -2,6 +2,15 @@ import style from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
+    let posts = [
+        {id:1, message:"Hi! how are you?", likeCount:"likes 12"},
+        {id:2, message:"Its my first post!", likeCount:"likes 22"},
+        {id:1, message:"Hi! how are you?", likeCount:"likes 12"},
+        {id:2, message:"Its my first post!", likeCount:"likes 22"},
+        {id:1, message:"Hi! how are you?", likeCount:"likes 12"},
+        {id:2, message:"Its my first post!", likeCount:"likes 22"}
+    ]
+    let postsItem = posts.map (post => <Post message={post.message} like={post.LikeCount}/>)
     return (
         <div className={style.posts}>
             My posts
@@ -11,9 +20,7 @@ const MyPosts = (props) => {
                 New post
             </div>
             <div className={style.posts}>
-                <Post message='Hi! how are you?' like='Like 16'/>
-                <Post message='Its my first post!' like='Like 12' />
-
+                {postsItem}
             </div>
 
 
